cbff196dcb993fc859b34b81001553fb1e567c42
Puschie <christoph286@googlemail.com>
2023-01-19 00:29:16 +0100

    Prepare blind implementation
    + add blind images
    + implement blind driver
    + create (empty) blind device
    + create (empty) blind definition

---
+++ drivers/blind/device.ts
@@ -0,0 +1,13 @@
+import { BaseDevice } from "../../lib/BaseDevice";
+import { CapabilityDefinition } from "../../types/CapabilityDefinition";
+
+class Device extends BaseDevice
+{
+	protected CapabilityDefinitions(): CapabilityDefinition
+	{
+
+	}
+
+}
+
+module.exports = Device;
